ddf724df096ede98db88fdb9c1979c56e8a33468
Syed Amaar Ahmad <saahmad@vt.edu>
2010-11-07 18:06:00 -0500

    further comments added

--- multi_queue_switch/test_switch_module_queue.c
+++ multi_queue_switch/test_switch_module_queue.c
@@ -32,11 +32,11 @@ int main(int argc, char *argv[])
 	In practice a config file will be read by the switch.*/
 
 
-	init_switch(argv[1]); //read config file, initialize the switch and the queues (and their associated semaphores)
+	init_switch(argv[1]); //read config. file, initialize the switch and the queues (and their associated semaphores)
 
 
-/**For tesing fins DATA frame via the switch suppose that the TCP module has to send some
- * data (test_data) to the IP module and ARP modules*/
+/**For tesing fins DATA frame via the switch. Suppose that the TCP module has to send some
+ * data (test_data) to the IP module and ARP module*/
 
 	test_data = 0x2f;
 
@@ -67,12 +67,13 @@ int main(int argc, char *argv[])
 	sem_post(&(swt_to_arp_bff->locked));
 	PRINT_DEBUG("\nTCP has sent %x and the ARP receives %x\n\n", test_data, *(unsigned char*)(read_fins.dataFrame.pdu));
 
+	free(next_dest);
 
-/**Now for tesing fins CONTROL frame via the switch suppose that a module has to send some query to the switch.
-The switch needs to send a reply request based on the search from its table. This side acts as a given module
-which is accessing the switch and sending query requests to it and updating its local cache based on the replies. It also offers
-the capability to run through the local cache and determine the list of destinations based on VCI and direction flag.
-Let us choose another module this time.
+/**Now for tesing fins CONTROL frame via the switch. Suppose that a module has to send some query to the switch.
+The switch needs to send a reply to a request based on the search from its table. This side acts like a given module
+which is accessing the switch, sending query requests to it and updating its local cache based on the replies. It also
+offers the capability to run through the local cache and determine the list of destinations based on VCI and direction flag.
+Let us choose another module for testing this time.
  */
 
 	for (i=0;i<1;i++){
